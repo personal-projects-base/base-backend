57c73cf64913b651c47880b1f56bf7de3ed8da07
Geovane-Araujo <geovane-araujo@outlook.com>
2023-07-17 23:14:28 -0300

    Adicionado registro automatico de serviço

--- src/main/java/com/potatotech/StarterApplication.java
+++ src/main/java/com/potatotech/StarterApplication.java
@@ -3,9 +3,11 @@ package com.potatotech;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableRabbit
 @SpringBootApplication
+@EnableFeignClients
 public class StarterApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/potatotech/basebackend/config/context/EnumConfigContext.java
+++ src/main/java/com/potatotech/basebackend/config/context/EnumConfigContext.java
@@ -5,5 +5,8 @@ public enum EnumConfigContext {
     DB_NAME,
     DB_USERNAME,
     DATABASE_SCHEMA_NAME,
-    DB_PASSWORD
+    DB_PASSWORD,
+    SECRET_JWT,
+    SERVER_PORT,
+    SERVICE_NAME
 }
